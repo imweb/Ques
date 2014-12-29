585305ba1b907f864f12f10784559680cd689e74
Daniel Yang <miniflycn@justany.net>
2014-12-30 01:17:03 +0800

    MVVM

--- components/helloworld/main.js
+++ components/helloworld/main.js
@@ -1,9 +1,23 @@
-var $ = require('jquery');
+var Q = require('Q');
 
 function init(container) {
-    $(container).on('click', function () {
-        alert('hello world!');
+    var q = new Q({
+        el: container,
+        data: {}
     });
+    q.setMessage = function (e) {
+        var values = [
+            'hello imweb!',
+            'hello world!',
+            'hello Tencent!',
+            'hello donaldyang!',
+            'hello qiqi!',
+            'hello futrue',
+            'hello QQ'
+        ], value = values[Math.random() * 7 | 0];
+        alert('现在值为:' + this.data.message + '; 要设置成:' + value)
+        this.set('message', value);
+    };
 }
 
 module.exports = {
--- gulp/lib/walker.js
+++ gulp/lib/walker.js
@@ -11,6 +11,21 @@ function tag(node, cb) {
   }
 }
 
+function text(node, cb) {
+  var children = node.children, l, child;
+  if (children && (l = children.length)) {
+    for (var i = 0; i < l; i++) {
+      child = children[i];
+      if (child.type === 'text') {
+        cb(child);
+      } else {
+        text(child, cb);
+      }
+    }
+  }
+}
+
 module.exports = {
-  tag: tag
+  tag: tag,
+  text: text
 };
