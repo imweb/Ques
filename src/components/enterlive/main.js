7d1df48de6e1d20681c8656d05a484206de1335c
Daniel Yang <miniflycn@justany.net>
2015-02-16 16:01:18 +0800

    fixed enterlive bug

--- src/components/enterlive/main.js
+++ src/components/enterlive/main.js
@@ -1,17 +1,8 @@
-var Q = require('Q');
-
-function init(container) {
-    Q.all({
-        el: container,
-        data: {},
-        methods: {
-            enterLive: function () {
-                alert('假设我们进入齐齐的直播大厅');
-            }
-        }
-    });
-}
-
 module.exports = {
-    init: init
-};
+    data: {},
+    methods: {
+        enterLive: function () {
+            alert('假设我们进入齐齐的直播大厅');
+        }
+    }
+}
