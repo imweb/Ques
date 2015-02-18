fb5d158c615396773f46d1040e724b56981515b2
Daniel Yang <miniflycn@justany.net>
2015-02-18 15:06:37 +0800

    refactor render.js

---
+++ src/components/r.js
@@ -0,0 +1,34 @@
+var Q = require('Q'),
+    $ = require('jquery');
+
+function Render(el, opts, data, options) {
+    var self = this;
+    $.extend((opts.filters = opts.filters || {}), options.filters);
+    this.el = el;
+    this.opts = opts;
+    this.data = data;
+    this.render();
+    this.timeout = setTimeout(function () {
+        self.bind();
+    }, 0);
+}
+var p = Render.prototype;
+p.render = function () {
+    $(this.el).html(tpl(this.data, this.opts));
+    return this;
+};
+p.bind = function () {
+    var opts = this.opts;
+    opts.el = this.el;
+    opts.data = this.data;
+    Q.all(opts);
+    return this;
+};
+p.unbind = function () {
+    clearTimeout(this.timeout);
+    return this;
+};
+
+module.exports = function (el, opts, data, options) {
+    return new Render(el, opts, data, options);
+};
--- src/components/render.js
+++ src/components/render.js
@@ -1,14 +1,8 @@
-var Q = require('Q'),
-    $ = require('jquery'),
+var Render = require('../r')
     tpl = require('./main.html'),
     opts = require('./main');
 
 // the render factory
-module.exports = function (el, data, filters) {
-    $.extend((opts.filters = opts.filters || {}), filters);
-    $(el).html(tpl(data, opts));
-    opts.el = el;
-    opts.data = data;
-    var res = Q.all(opts);
-    return res.length > 1 ? res : res[0];
+module.exports = function (el, data, options) {
+    return Render(el, opts, data, options)
 };
