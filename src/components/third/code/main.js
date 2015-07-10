c336e556f09c51d1e7fc535c21240a4689cfae28
Daniel Yang <miniflycn@justany.net>
2015-07-10 17:36:46 +0800

    remove dependence

--- src/components/third/code/main.js
+++ src/components/third/code/main.js
@@ -1,11 +1,13 @@
-var $ = require('jquery');
 
 module.exports = {
     bind: function () {
-        var el = this.el;
-        $.getScript('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js', function () {
+        var el = this.el,
+            script = document.createElement('script');
+        script.onload = function () {
             hljs.highlightBlock(el);
-        });
+        }
+        script.src = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js';
+        document.body.appendChild(script);
     },
     unbind: function () {}
 };
