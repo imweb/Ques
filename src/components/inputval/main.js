70507558c9d57b1991f727a0bdd6698f1327f109
Daniel Yang <miniflycn@justany.net>
2015-07-18 21:53:13 +0800

    fixed css bug

--- src/components/inputval/main.js
+++ src/components/inputval/main.js
@@ -3,9 +3,18 @@ var filters = require('filters');
 module.exports = {
     methods: {
         submit: function () {
-            this.$emit('submit', this.curVal);
-            this.$set('curVal', '');
-            this.hide();
+            if (!this.curVal) {
+                this.$set('focus', true);
+            } else {
+                this.$emit('submit', this.curVal);
+                this.$set('curVal', '');
+                this.hide();
+            }
+        },
+        show: function () {
+            // use super.show
+            this.constructor.super.options.methods.show.call(this);
+            this.$set('focus', true);
         }
     },
     directives: {
