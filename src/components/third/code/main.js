5b7286617286e60024fa2dc56d8f9f45e3c9fc53
Daniel Yang <miniflycn@justany.net>
2015-10-20 21:30:39 +0800

    build new version

--- src/components/third/code/main.js
+++ src/components/third/code/main.js
@@ -1,16 +1,30 @@
-var _ = require('Q')._;
+/* globals hljs: false */
+var $ = require('jquery'),
+    _ = require('Q')._;
 
-module.exports = {
-    bind: function () {
-        var el = this.el,
-            script = document.createElement('script');
+function load() {
+    var defer = $.Deferred(),
+        script;
+    if (window.hljs) {
+        defer.resolve(hljs);
+    } else {
+        script = document.createElement('script');
         script.onload = function () {
-            /* globals hljs: false */
-            hljs.highlightBlock(el);
-            _.addClass(el, 'show');
+            defer.resolve(hljs);
         };
         script.src = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js';
         document.body.appendChild(script);
+    }
+    return defer.promise();
+}
+
+module.exports = {
+    bind: function () {
+        var el = this.el;
+        load().done(function (hljs) {
+            hljs.highlightBlock(el);
+            _.addClass(el, 'show');
+        });
     },
     unbind: function () {}
 };
