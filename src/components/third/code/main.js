a6cff49678c53c99682d8a61f7083dceacfb86f7
Daniel Yang <miniflycn@justany.net>
2015-10-20 21:41:30 +0800

    build

--- src/components/third/code/main.js
+++ src/components/third/code/main.js
@@ -1,27 +1,24 @@
 /* globals hljs: false */
-var $ = require('jquery'),
-    _ = require('Q')._;
+var _ = require('Q')._;
 
-function load() {
-    var defer = $.Deferred(),
-        script;
+function load(cb) {
+    var script;
     if (window.hljs) {
-        defer.resolve(hljs);
+        cb(hljs);
     } else {
         script = document.createElement('script');
         script.onload = function () {
-            defer.resolve(hljs);
+            cb(hljs);
         };
         script.src = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js';
         document.body.appendChild(script);
     }
-    return defer.promise();
 }
 
 module.exports = {
     bind: function () {
         var el = this.el;
-        load().done(function (hljs) {
+        load(function (hljs) {
             hljs.highlightBlock(el);
             _.addClass(el, 'show');
         });
