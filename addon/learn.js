a2d0d76eb2e80558b6123b4897f6e11268a4927e
Daniel Yang <miniflycn@justany.net>
2015-12-13 17:36:59 +0800

    fishiness learning module

--- addon/learn.js
+++ addon/learn.js
@@ -2,7 +2,8 @@ var path = require('path')
   , fs = require('fs')
   , url = require('url')
   , config = require('../config')
-  , cwd = process.cwd();
+  , cwd = process.cwd()
+  , middlewarePipe = require('middleware-pipe');
 
 function _getFile(query) {
   if (!query) query = '';
@@ -25,6 +26,7 @@ function index(req, res, next, urlObj) {
     fs.existsSync(openFile) ?
       fs.readFileSync(openFile, 'utf-8')
         .replace(/\r?\n/g, '\\n')
+        .replace(/\\'/g, '\\\\\'')
         .replace(/'/g, "\\'")
         .replace(/\<\/script\>/g, "</' + 'script>") :
       ''
@@ -48,6 +50,35 @@ function submit(req, res, next, urlObj) {
   });
 }
 
+function file(req, res, next, urlObj) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  var p = decodeURIComponent(urlObj.query.split('=')[1]),
+    result = fs.readdirSync(path.resolve(__dirname, '../src/', p))
+    .map(function (file) {
+      var i = file.lastIndexOf('.');
+      if (~i) {
+        return [
+          '<li class="file ext_' + file.substring(i + 1) + '">',
+            '<a href="#" rel="' + p + file + '">' + file + '</a>',
+          '</li>'
+        ].join('');
+      } else {
+        return [
+          '<li class="directory collapsed">',
+            '<a href="#" rel="' + p + file + '/">' + file + '</a>',
+          '</li>'
+        ].join('');
+      }
+    });
+  result.unshift('<ul class="jqueryFileTree" style="display: none;">');
+  result.push('</ul>');
+  res.end(result.join(''));
+}
+
+var defaultMiddle = middlewarePipe(path.join(__dirname, './learn'));
+
 function middleware(req, res, next) {
   var urlObj = url.parse(req.url);
   switch (urlObj.pathname) {
@@ -55,6 +86,10 @@ function middleware(req, res, next) {
       return index(req, res, next, urlObj);
     case '/submit':
       return submit(req, res, next, urlObj);
+    case '/file':
+      return file(req, res, next, urlObj);
+    default:
+      return defaultMiddle(req, res, next)
   }
   next();
 }
